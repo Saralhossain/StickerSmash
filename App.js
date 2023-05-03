b55871be5eb1ff411f81baac230948e1ddb61921
Saralhossain <alamdar.hussain0007@gmail.com>
2023-05-03 06:08:00 -0700

    Adding add feature of save media/Taking screenshots

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
+
+//Libraries
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import { useState , useRef } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+
+
+//Components
 import ImageViewer from './components/imageViewer';
 import Button from './components/Button';
 import CircleButton from './components/circleButton';
@@ -11,16 +18,32 @@ import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 
+
+//Assests
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const imageRef = useRef();
+
+  //Save media on device
+  // const onSaveImageAsync = async () => {
+    
+  // };
+
+  //Uploading Image
   const [selectedImage , setSelectedImage] = useState(null);
+  //Show Button Options
   const [showAppOptions , setShowAppOptions]=useState(false);
   const [isModalVisible , setIsModalVisible]=useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
 
+  //Permission for taking screenshots
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  if (status === null) {
+    requestPermission();
+  }
 
-  const pickImageAsync= async() => {
+  const pickImageAsync = async() => {
       let result =  await ImagePicker.launchImageLibraryAsync({
         allowsEditing:true,
         quality:1,
@@ -34,12 +57,14 @@ export default function App() {
       else{
         alert('You did not select any image.');
       }
-    }
+    };
 
+      //If onReset will Call it will turnon select choose a photo/use this photo buttons 
     const onReset = () => {
       setShowAppOptions(false);
     };
 
+    //this options shows the sticker list 
     const onAddSticker = () => {
       setIsModalVisible(true);
       // we will implement this later
@@ -47,10 +72,23 @@ export default function App() {
 
     const onModalClose= () =>{
       setIsModalVisible(false);
-    }
+    };
   
+    //Save media on device
     const onSaveImageAsync = async () => {
-      // we will implement this later
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+  
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
+      }
     };
 
 
@@ -58,9 +96,11 @@ export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
     {/* <View style={styles.container}> */}
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
-        {pickedEmoji !==null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null }
+      <View  style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+          {pickedEmoji !==null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null }
+        </View>
       </View>
       { showAppOptions ?
        (
