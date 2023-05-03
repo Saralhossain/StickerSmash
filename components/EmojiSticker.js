8a79e2664dec13ddab5ff8360f7eea9555a826d9
Saralhossain <alamdar.hussain0007@gmail.com>
2023-05-03 05:24:30 -0700

    Adding pan gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,6 +1,8 @@
 import {View , Image } from 'react-native';
 // import Animated from 'react-native-reanimated';
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { TapGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
+// import {  , TapGestureHandler} from "react-native-gesture-handler";
+
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -11,12 +13,17 @@ import Animated, {
 
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
-
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSpicker({imageSize , stickerSource}){
+    //initial posi of sticker
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
+
     const scaleImage = useSharedValue(imageSize);
 
-
+    //increase the size of sticker when On tap
     const onDoubleTap = useAnimatedGestureHandler({
         onActive: () => {
           if (scaleImage.value) {
@@ -25,23 +32,55 @@ export default function EmojiSpicker({imageSize , stickerSource}){
         },
       });
 
+      //initialize size of sticker
       const imageStyle = useAnimatedStyle(() => {
         return {
           width: withSpring(scaleImage.value),
           height: withSpring(scaleImage.value),
         };
       });
+
+      //After onLongPress the value of x,y position of sticker will be stored
+      const onDrag = useAnimatedGestureHandler({
+        //Starting Value
+        onStart: (event, context) => {
+          context.translateX = translateX.value;
+          context.translateY = translateY.value;
+        },
+        //Onactive callback the value x,y , moving position of sticker
+        onActive: (event, context) => {
+          translateX.value = event.translationX + context.translateX;
+          translateY.value = event.translationY + context.translateY;
+        },
+      });
+      
+
+      //change the sticker's position when the gesture is active.
+      const containerStyle = useAnimatedStyle(() => {
+        return {
+          transform: [
+            {
+              translateX: translateX.value,
+            },
+            {
+              translateY: translateY.value,
+            },
+          ],
+        };
+      });
       
         return (
-            <View style ={{top: -250 , left:100}}>
-            <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-                <AnimatedImage
-                    source={stickerSource}
-                    sizeMode="contain"
-                    style={[imageStyle, { width: imageSize, height: imageSize }]}
-                />
-                </TapGestureHandler>
-            </View>
+            <PanGestureHandler onGestureEvent={onDrag}>
+                <AnimatedView style ={[containerStyle , {top: -250 , left:100}]} >
+                <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                    <AnimatedImage
+                        source={stickerSource}
+                        sizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                    </TapGestureHandler>
+                </AnimatedView>
+            </PanGestureHandler>
         );
 
 }
